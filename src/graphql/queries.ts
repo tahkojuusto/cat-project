7d6904e5a6685150d96eb039cdbb7e23a443e8e0
tahkojuusto <6095076+tahkojuusto@users.noreply.github.com>
2020-02-29 16:49:46 +0200

    :crocodile: Add GraphQL (AppSync)

---
+++ src/graphql/queries.ts
@@ -0,0 +1,32 @@
+// tslint:disable
+// this is an auto generated file. This will be overwritten
+
+export const getBreed = /* GraphQL */ `
+  query GetBreed($id: ID!) {
+    getBreed(id: $id) {
+      id
+      name
+      description
+      temperament
+      origin
+    }
+  }
+`;
+export const listBreeds = /* GraphQL */ `
+  query ListBreeds(
+    $filter: ModelBreedFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listBreeds(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        description
+        temperament
+        origin
+      }
+      nextToken
+    }
+  }
+`;
